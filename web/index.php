dfabc69ef852ae99f3fc23728a56b98726185865
Joshua <jy2xj@virginia.edu>
2017-04-02 22:28:36 -0400

    move files into web

---
+++ web/index.php
@@ -0,0 +1,26 @@
+<?php
+
+require('../vendor/autoload.php');
+
+$app = new Silex\Application();
+$app['debug'] = true;
+
+// Register the monolog logging service
+$app->register(new Silex\Provider\MonologServiceProvider(), array(
+  'monolog.logfile' => 'php://stderr',
+));
+
+// Register view rendering
+$app->register(new Silex\Provider\TwigServiceProvider(), array(
+    'twig.path' => __DIR__.'/views',
+));
+
+// Our web handlers
+
+$app->get('/', function() use($app) {
+  $app['monolog']->addDebug('logging output.');
+  return $app['twig']->render('index.html');
+  //return str_repeat('Hello', getenv('TIMES'));
+});
+
+$app->run();
